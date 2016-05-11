24368919811e5577337c3b69712fcd4eb521d815
Mohssen Fathi <mmohssenfathi@gmail.com>
2016-05-11 12:59:16 -0500

    Sync

--- Pod/Classes/Filters/MTLHarrisCornerDetection.swift
+++ Pod/Classes/Filters/MTLHarrisCornerDetection.swift
@@ -145,3 +145,50 @@ class MTLHarrisCornerDetectionOutputFilter: MTLFilter {
         commandEncoder.setTexture(originalTexture, atIndex: 2)
     }
 }
+
+
+
+struct HarrisCornerDetectionUniforms {
+    var sensitivity: Float = 0.5;
+}
+
+public
+class MTLHarrisCornerDetectionFilter: MTLFilter {
+    
+    var uniforms = HarrisCornerDetectionUniforms()
+    
+    public var sensitivity: Float = 0.5 {
+        didSet {
+            clamp(&sensitivity, low: 0, high: 1)
+            needsUpdate = true
+            update()
+        }
+    }
+    
+    public var threshold: Float = 0.5 {
+        didSet {
+            clamp(&threshold, low: 0, high: 1)
+            needsUpdate = true
+            update()
+        }
+    }
+    
+    public init() {
+        super.init(functionName: "harrisCornerDetection")
+        title = "Harris Corner Detection"
+        properties = [MTLProperty(key: "sensitivity", title: "Sensitivity"),
+                      MTLProperty(key: "threshold"  , title: "Threshold"  )]
+        
+        update()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func update() {
+        if self.input == nil { return }
+        uniforms.sensitivity = sensitivity
+        uniformsBuffer = device.newBufferWithBytes(&uniforms, length: sizeof(HarrisCornerDetectionUniforms), options: .CPUCacheModeDefaultCache)
+    }
+}
