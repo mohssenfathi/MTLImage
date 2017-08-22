cd735abdbce18175e8de450eefb83caf6caa53cb
Mohssen Fathi <mmohssenfathi@gmail.com>
2017-08-22 07:39:14 -0500

    Added podspec, Package.swift

---
+++ MTLImage/Deployment/Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  MTLImage
+//
+//  Created by Mohssen Fathi on 8/22/17.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "MTLImage",
+    dependencies : [],
+    exclude: ["Tests"]
+)
+
